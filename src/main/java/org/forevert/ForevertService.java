0552a7c1871ebee42880524e0cea92b840de1c19
jacek <jacek.golek@web.de>
2014-04-16 16:55:21 +0200

    added ace-editor

--- src/main/java/org/forevert/ForevertService.java
+++ src/main/java/org/forevert/ForevertService.java
@@ -2,13 +2,18 @@ package org.forevert;
  
 import java.io.IOException;
 
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 
 import org.forevert.model.Page;
 import org.forevert.unit.html.BuildAHtmlPageFlow;
+import org.forevert.unit.html.HtmlRenderer;
 import org.forevert.unit.io.PageIo;
 import org.forevert.unit.io.PageIoException;
 import org.forevert.unit.io.PageTemplate;
@@ -19,20 +24,37 @@ public class ForevertService {
 
 	@GET
 	public Response getStartPage() throws PageIoException, IOException {
-		return this.buildAHtmlPage("test");
+		return this.showPage("test");
 	}
 	
 	@GET
 	@Path("{pageName}")
-	public Response buildAHtmlPage(@PathParam("pageName") String pageName) throws PageIoException, IOException {
-		//if(true) return Response.status(200).entity("test").build();
+	public Response buildAHtmlPage(@PathParam("pageName") String pageName, @Context UriInfo info ) throws PageIoException, IOException {
+		
+		boolean editMode = info.getQueryParameters().containsKey("edit");
+		
+		if(editMode){
+			return showEditor(pageName);
+		}else{
+			return showPage(pageName);
+		}
+	}
+
+	public Response showPage(String pageName) throws PageIoException, IOException {
 		
 		Page 		 page     = PageIo.find(pageName);
 		PageTemplate template = PageTemplateIo.findByTemplateName("page.soy");
 
-		String html = BuildAHtmlPageFlow.execute(page, template);
-
+		String html = BuildAHtmlPageFlow.execute(page, template );
+		return Response.status(200).entity(html).build();
+	}
+	
+	public Response showEditor(@PathParam("pageName") String pageName) throws PageIoException, IOException {
 		
+		Page 		 page     = PageIo.find(pageName);
+		PageTemplate template = PageTemplateIo.findByTemplateName("edit.soy");
+
+		String html = HtmlRenderer.renderHtml(page, template, page.content);
 		return Response.status(200).entity(html).build();
 	}
 }
